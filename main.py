ace7967674177f98d3495f1bc29809da48c11d08
ValentinTugot <valentin.tugot@gmail.com>
2022-11-21 11:52:45 +0100

    Ajout d'une superclasse pour eviter les répétitions

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from tkinter import *
 from PIL import Image,ImageTk
 
-routerList=[]
-
 
 #Definition de la fenêtre et du canva
 root = Tk()
@@ -29,133 +27,135 @@ def setDefault():
 class Selector():
     def __init__(self) -> None:
         self.etat = "Selection"
-     
-     
+
+routerList = []
+clientList = []
+switchList = []
+
 class Elements():
-    
     def __init__(self) -> None:
-        self.routerList = []
-        self.clientList = []
-        self.SwitchList = []
-         
-    class Router():
-        def __init__(self) -> None:
-            self.img = (Image.open("assets/router.png"))
-            self.resize = self.img.resize((100,100))
-            self.image = ImageTk.PhotoImage(self.resize)
-            self.id = len(routerList)
-            self.displayname=f'router{self.getID()}'
-            self.edit_menu = Menu(root)
-            self.edit_menu.add_command(label="Rename",command=self.rename_menu)
-            self.edit_menu.add_command(label="Delete",command=self.remove)
+        self.edit_menu = Menu(root)
+        self.edit_menu.add_command(label="Rename",command=self.rename_menu)
+        self.edit_menu.add_command(label="Delete",command=self.remove)
         
-        def getID(self):
-            return self.id
-
-        def place(self,x,y):
-            self.placeimg = canva.create_image(x,y,image=self.image,anchor=CENTER)
-            self.name = canva.create_text(x,y+40,text=self.displayname)
-            routerList.append(self.displayname)
-            canva.tag_bind(self.placeimg,"<Button-3>",self.edit)
-            
-        
-        def edit(self,e):
+    def getID(self):
+        return self.id
+    
+    def rename_menu(self):
+        self.rename_window = Toplevel(root)
+        self.rename_window.title("Rename")
+        self.rename_window.geometry("200x150")
+        self.rename_window.resizable(False,False)
+        self.rename_entry = Entry(self.rename_window)
+        self.rename_entry.insert(0,canva.itemcget(self.name,'text'))
+        self.rename_entry.place(x=26,y=50)
+        self.confirm_button = Button(self.rename_window,text="Rename",command=self.rename)
+        self.confirm_button.pack(side="bottom")
+    
+    def edit(self,e):
+        try:
             #Bind la popup du menu clique droit
-            try:
-                self.edit_menu.tk_popup(e.x_root,e.y_root)
-            finally:
-                #Revient à la normal après l'utilisation
-                self.edit_menu.grab_release()
+            self.edit_menu.tk_popup(e.x_root,e.y_root)
+        finally:
+            #Revient à la normal après l'utilisation
+            self.edit_menu.grab_release()
+    
+    def rename(self):
+        self.new_name = self.rename_entry.get()
+        canva.itemconfig(self.name,text=self.new_name)
+        self.rename_window.destroy()
+    
+    def remove(self):
+        canva.delete(self.placeimg)
+        canva.delete(self.name)
+            
+class Router(Elements):
+    def __init__(self) -> None:
+        super().__init__()
+        self.img = (Image.open("assets/router.png"))
+        self.resize = self.img.resize((100,100))
+        self.image = ImageTk.PhotoImage(self.resize)
+        self.id = len(routerList)
+        self.displayname=f'router{self.getID()}'
+    
+    def place(self,x,y):
+        self.placeimg = canva.create_image(x,y,image=self.image,anchor=CENTER)
+        self.name = canva.create_text(x,y+40,text=self.displayname)
+        routerList.append(self.displayname)
+        self.index = routerList.index(self.displayname)
+        canva.tag_bind(self.placeimg,"<Button-3>",super().edit)
         
-        def rename_menu(self):
-            self.rename_window = Toplevel(root)
-            self.rename_window.title("Rename")
-            self.rename_window.geometry("200x150")
-            self.rename_entry = Entry(self.rename_window)
-            self.rename_entry.insert(0,canva.itemcget(self.name,'text'))
-            self.rename_entry.pack(side="top")
-            self.confirm_button = Button(self.rename_window,text="Rename",command=self.rename)
-            self.confirm_button.pack(side="bottom")
+    def rename(self):
+        super().rename()
+        routerList[self.index] = self.new_name
+    
+    def remove(self):
+        super().remove()
+        routerList.pop(self.index)
+    
         
-        def rename(self):
-            self.new_name = self.rename_entry.get()
-            canva.itemconfig(self.name,text=self.new_name)
-            self.rename_window.destroy()
+class Client(Elements):
+    def __init__(self) -> None:
+        super().__init__()
+        self.img = (Image.open("assets/client.png"))
+        self.resize = self.img.resize((100,100))
+        self.image = ImageTk.PhotoImage(self.resize)
+        self.id = len(clientList)
+        self.displayname=f'client{self.getID()}'
         
-        def remove(self):
-            canva.delete(self.placeimg)
-            canva.delete(self.name)
+    def place(self,x,y):
+        self.placeimg = canva.create_image(x,y,image=self.image,anchor=CENTER)
+        self.name = canva.create_text(x,y+60,text=self.displayname)
+        clientList.append(self.displayname)
+        self.index = clientList.index(self.displayname)
+        canva.tag_bind(self.placeimg,"<Button-3>",self.edit)
+    
+    def rename(self):
+        super().rename()
+        clientList[self.index] = self.new_name
+    
+    def remove(self):
+        super().remove()
+        clientList.pop(self.index)
             
+class Switch(Elements):
+    def __init__(self) -> None:
+        super().__init__()
+        self.img = (Image.open("assets/switch.png"))
+        self.resize = self.img.resize((100,100))
+        self.image = ImageTk.PhotoImage(self.resize)
+        self.id = len(switchList)
+        self.displayname=f'switch{self.getID()}'   
         
-    class Client():
-        def __init__(self) -> None:
-            self.img = (Image.open("assets/client.png"))
-            self.resize = self.img.resize((100,100))
-            self.image = ImageTk.PhotoImage(self.resize)
-            self.id = 0
-            self.edit_menu = Menu(root)
-            self.edit_menu.add_command(label="Rename")
-            self.edit_menu.add_command(label="Delete")
-            
-        def getID(self):
-            return self.id
-            
-        def place(self,x,y):
-            canva.create_image(x,y,image=self.image,anchor=CENTER)
-            self.name = canva.create_text(x,y+60,text=f'client{self.getID()}')
-            canva.tag_bind(self.placeimg,"<Button-3>",self.edit)
-            self.id +=1
+    def place(self,x,y):
+        self.placeimg = canva.create_image(x,y,image=self.image,anchor=CENTER)
+        self.name = canva.create_text(x-15,y+35,text=self.displayname)
+        switchList.append(self.displayname)
+        self.index= switchList.index(self.displayname)
+        canva.tag_bind(self.placeimg,"<Button-3>",self.edit)
         
-        def edit(self,e):
-            #Bind la popup du menu
-            try:
-                self.edit_menu.tk_popup(e.x_root,e.y_root)
-            finally:
-                #Après l'utilisation de la popup, revient à la normal
-                self.edit_menu.grab_release()
+    def rename(self):
+        super().rename()
+        switchList[self.index] = self.new_name
+    
+    def remove(self):
+        super().remove()
+        switchList.pop(self.index)
             
-
-    class Switch():
-        def __init__(self) -> None:
-            self.img = (Image.open("assets/switch.png"))
-            self.resize = self.img.resize((100,100))
-            self.image = ImageTk.PhotoImage(self.resize)
-            self.id = 0
-            self.edit_menu = Menu(root)
-            self.edit_menu.add_command(label="Rename")
-            self.edit_menu.add_command(label="Delete")
-            
-        def getID(self):
-            return self.id   
-            
-        def place(self,x,y):
-            canva.create_image(x,y,image=self.image,anchor=CENTER)
-            self.name = canva.create_text(x-15,y+35,text=f'switch{self.getID()}')
-            canva.tag_bind(self.placeimg,"<Button-3>",self.edit)
-            self.id +=1
-            
-        def edit(self,e):
-            try:
-                self.edit_menu.tk_popup(e.x_root,e.y_root)
-            finally:
-                self.edit_menu.grab_release()
-            
-        
 
 #Instancie les objets
 sel = Selector()
-router = Router()
-client = Client()
-switch = Switch()
 
 #Fonction qui place les elements en fonction de l'état du selecteur
 def placeElmt(e):
     if sel.etat == "Router":
-        rout = Router()
-        rout.place(e.x,e.y)
+        router = Router()
+        router.place(e.x,e.y)
     elif sel.etat == "Client":
+        client = Client()
         client.place(e.x,e.y)
     elif sel.etat == "Switch":
+        switch = Switch()
         switch.place(e.x,e.y)
     elif sel.etat == "Selection":
         return
