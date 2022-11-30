2ae22adc2d17b43de7be459a8a9c1f3bda24c10a
ValentinTugot <valentin.tugot@gmail.com>
2022-11-30 20:02:54 +0100

    Ajout de commentaires + help windows

--- main.py
+++ main.py
@@ -1,22 +1,54 @@
 from tkinter import *
 from PIL import Image,ImageTk
 from tkinter import filedialog
-from tkinter.filedialog import askopenfile
 
-#Definition de la fenêtre et du canva
+#-----------------------------------#
+#Définition de la fenêtre principale
+#-----------------------------------#
 root = Tk()
 root.geometry("1280x720")
-root.title('Schama')
+root.title('Network Plan')
+
+#-----------------------------------#
+#Définition de la fenêtre d'aide
+#-----------------------------------#
+help_window = Toplevel(root)
+help_window.title("Help")
+help_window.geometry("500x500")
+help_window.resizable(False,False)
+label_text= """
+
+Liste des commandes disponibles:
+
+Clique Gauche - Permet de placer les éléments après les avoir selectionnés.
+R - Séléctionne les Routers
+C - Séléctionne les clients
+S - Sélectionne les Switchs
+N - Enlever la sélection
+
+La sélection peut également s'éffectuer depuis le menu \"Add".
+
+Un clique droit sur un élément permet de le modifier:
+- Renommer
+- Supprimer
+- Changer son icône
+
+Il faut être en mode Séléction pour déplacer les éléments.
+            """
+help_window_label = Label(help_window,text=label_text)
+help_window_label.place(x=30,y=100)
+help_window_button = Button(help_window,text="OK",command=help_window.destroy)
+help_window_button.pack(side=BOTTOM)
+
+#-----------------------------------#
+#Définition du caneva
+#-----------------------------------#
 canva = Canvas(root,width=1920,height=1080,bg="ivory")
 canva.pack()
 
-def get_image_x(img):
-    return canva.coords(img)[0]
-
-def get_image_y(img):
-    return canva.coords(img)[1]
-
-#Changer l'état du selecteur
+#-----------------------------------#
+#Fonctions permettant le changement de séléction
+#-----------------------------------#
 def setRouter():
     sel.etat = "Router"
     
@@ -29,34 +61,43 @@ def setSwitch():
 def setDefault():
     sel.etat = "Selection"
     
-def setLink():
-    sel.etat="Link"
-    
-
-#Definition des objets
+#-----------------------------------#
+#Définition de l'objet sélécteur
+#-----------------------------------#
 class Selector():
     def __init__(self) -> None:
         self.etat = "Selection"
 
-#Listes des objets crées
+#-----------------------------------#
+#Différentes listes qui répertories les objets placés
+#-----------------------------------#
 routerList = []
 clientList = []
 switchList = []
 
+#-----------------------------------------------------------------#
+#Définition des objets
+#-----------------------------------------------------------------#
+
+#-----------------------------------#
+#Définition de la superclasse Elements
+#-----------------------------------#
 class Elements():
     def __init__(self) -> None:
+        #Menu d'édition (Clique droit)
         self.edit_menu = Menu(root)
         self.edit_menu.add_command(label="Rename",command=self.rename_menu)
         self.edit_menu.add_command(label="Delete",command=self.remove)
         self.edit_menu.add_command(label="Icon",command=self.icon_menu)
-        
+    
+    #Fonction qui place les éléments sur le schéma
     def place(self,x,y):
         self.placeimg = canva.create_image(x,y,image=self.image,anchor=CENTER)
         self.name = canva.create_text(x,y+60,text=self.displayname)
         canva.tag_bind(self.placeimg,"<Button-3>",self.edit)
         canva.tag_bind(self.placeimg,"<B1-Motion>",self.move)
-        canva.tag_bind(self.placeimg,"<Button-1>",self.link)
-        
+    
+    #Menu permettant de renommer l'élément
     def rename_menu(self):
         self.rename_window = Toplevel(root)
         self.rename_window.title("Rename")
@@ -68,6 +109,7 @@ class Elements():
         self.confirm_button = Button(self.rename_window,text="Rename",command=self.rename)
         self.confirm_button.pack(side="bottom")
     
+    #Menu permettant de gérer l'icone
     def icon_menu(self):
         self.icon_window = Toplevel(root)
         self.icon_window.title("Icon Informations")
@@ -78,7 +120,8 @@ class Elements():
         self.canvaicon.create_image(0,0,image=self.image,anchor=NW)
         self.upload_button = Button(self.icon_window,text="Modify Image",command=self.set_icon)
         self.upload_button.place(x=45,y=100)
-        
+    
+    #Fonction affichant le menu d'édition    
     def edit(self,e):
         try:
             #Bind la popup du menu clique droit
@@ -86,30 +129,36 @@ class Elements():
         finally:
             #Revient à la normal après l'utilisation
             self.edit_menu.grab_release()
-    
+
+    #Fonction permettant de renommer l'élément placé
     def rename(self):
         self.new_name = self.rename_entry.get()
         canva.itemconfig(self.name,text=self.new_name)
         self.rename_window.destroy()
     
+    #Fonction permettant de supprimer l'élément placé
     def remove(self):
         canva.delete(self.placeimg)
         canva.delete(self.name)
     
+    #Fonction permettant de redéfinir un nouvel icône à l'élément placé
     def set_icon(self):
-        self.image_type = [('Png Files', '*.png'),('Jpg Files', '*jpg')]
-        self.filename= filedialog.askopenfilename(filetypes=self.image_type)
+        self.image_type = [('Png Files', '*.png'),('Jpg Files', '*jpg')] #Type d'extension pour l'icône
+        self.filename= filedialog.askopenfilename(filetypes=self.image_type) #On ouvre une fenêtre permettant à l'utilisateur de choisir une image sur son PC
         self.image = ImageTk.PhotoImage((Image.open(self.filename)).resize((100,100)))
         canva.itemconfig(self.placeimg,image=self.image)
         self.icon_window.destroy()
-        
+    
+    #Fonction permettant le déplacement de l'élément    
     def move(self,e):
         if sel.etat != "Selection":
             return
         canva.coords(self.placeimg,e.x,e.y)
         canva.coords(self.name,e.x,e.y+60)
-    
-    
+        
+#-----------------------------------#
+#Définition de la classe Router (héritant de la superclasse)
+#-----------------------------------#    
 class Router(Elements):
     def __init__(self) -> None:
         super().__init__()
@@ -129,8 +178,10 @@ class Router(Elements):
     
     def remove(self):
         super().remove()
-        routerList.pop(self.index)
-          
+        routerList.remove(self.displayname)
+#-----------------------------------#
+#Définition de la classe Client (héritant de la superclasse)
+#-----------------------------------#          
 class Client(Elements):
     def __init__(self) -> None:
         super().__init__()
@@ -149,8 +200,11 @@ class Client(Elements):
     
     def remove(self):
         super().remove()
-        clientList.pop(self.index)
-            
+        clientList.remove(self.displayname)
+ 
+#-----------------------------------#
+#Définition de la classe Switch (héritant de la superclasse)
+#-----------------------------------#               
 class Switch(Elements):
     def __init__(self) -> None:
         super().__init__()
@@ -169,22 +223,17 @@ class Switch(Elements):
     
     def remove(self):
         super().remove()
-        switchList.pop(self.index)
-    
-class Cable():
-    def __init__(self,x1,y1,x2,y2) -> None:
-        self.x1,self.y1 = x1,y1
-        self.x2,self.y2 = x2,y2
-    
-    def place(self):
-        canva.create_line(self.x1,self.y1,self.x2,self.y2,fill='black',width=5)
+        switchList.remove(self.displayname)
+         
         
-        
-        
-#Instancie le selecteur
+#-----------------------------------#
+#On instancie le selecteur
+#-----------------------------------#    
 sel = Selector()
 
-#Fonction qui place les elements en fonction de l'état du selecteur
+#-----------------------------------#
+#Fonction plaçant les éléments en fonction de l'état du selecteur
+#-----------------------------------#    
 def placeElmt(e):
     if sel.etat == "Router":
         router = Router()
@@ -198,7 +247,9 @@ def placeElmt(e):
     elif sel.etat == "Selection":
         return
     
-#Selection des elements avec les touches du clavier
+#-----------------------------------#
+#Fonction permettant le changement de selecteur avec le clavier
+#-----------------------------------#    
 def changeSelector(e):
     if not e.char:
         return
@@ -208,12 +259,12 @@ def changeSelector(e):
         sel.etat = "Switch"
     elif e.char == "r":
         sel.etat = "Router"
-    elif e.char == "l":
-        sel.etat = "Link"
     elif e.char == "n":
         sel.etat="Selection"
     
-#Gestion du menu (toolbar)
+#-----------------------------------#
+#Définition du Menu (toolbar)
+#-----------------------------------#    
 toolbar = Menu(root)
 root.config(menu=toolbar)
 add_menu = Menu(toolbar)
@@ -221,10 +272,11 @@ add_menu.add_command(label="Selection",command=setDefault)
 add_menu.add_command(label="Router",command=setRouter)
 add_menu.add_command(label="Client",command=setClient)
 add_menu.add_command(label="Switch",command=setSwitch)
-add_menu.add_command(label="Link",command=setLink)
-toolbar.add_cascade(label="Ajouter",menu=add_menu)
+toolbar.add_cascade(label="Add",menu=add_menu)
 
-#Gestion des callbacks
+#-----------------------------------#
+#Gestion des Callbacks
+#-----------------------------------#    
 root.bind("<ButtonPress-1>",placeElmt)
 root.bind("<KeyPress>",changeSelector)
 root.mainloop()
